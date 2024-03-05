6a309f36e96f10172e3b23f487395099393445d8
Mahanish-Baruah <mahanishbaruah@gmail.com>
2024-03-05 21:51:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,12 +12,8 @@ const io = new Server(server);
 app.use(cors());
 app.use(router);
 
-app.use((req, res, next) => {
+io.on("connection", (res, socket) => {
     res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-
-io.on("connection", (socket) => {
     console.log(`Connected: ${socket.id}`);
 
     socket.on("join", ({ name, room }, callback) => {
